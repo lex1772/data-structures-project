13dabdabcc7eaaa9dee3d48d54ffbb87cc389d42
lexa1 <lexa1772@gmail.com>
2023-03-29 12:41:48 +0300

    ДЗ 6: Добавлены в класс LinkedList два метода:
    to_list() - возвращает список с данными, содержащимися в односвязном списке LinkedList
    get_data_by_id() - возвращает первый найденный в LinkedList словарь с ключом 'id', значение которого равно переданному в метод значению.
    Добавлен в метод get_data_by_id() блок try/except для перехвата ошибки TypeError и печати в консоль сообщения о неподходящем формате данных.
    
    Тестирование:
    
    Добавлены тесты для методов to_list() и get_data_by_id()

--- src/linked_list.py
+++ src/linked_list.py
@@ -10,6 +10,10 @@ class LinkedList:
     """Класс для односвязного списка"""
     head = None
     end = None
+    elements_list = []
+
+    def __init__(self):
+        self.elements_list = []
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
@@ -20,9 +24,7 @@ class LinkedList:
         else:
             tmp.next_node = self.head
         self.head = tmp
-
-
-
+        self.elements_list.insert(0, data)
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
@@ -33,6 +35,7 @@ class LinkedList:
         else:
             self.end.next_node = tmp
         self.end = tmp
+        self.elements_list.append(data)
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -47,3 +50,19 @@ class LinkedList:
 
         ll_string += ' None'
         return ll_string.lstrip()
+
+    def to_list(self):
+        return self.elements_list
+
+    def get_data_by_id(self, key):
+        while key <= len(self.elements_list):
+            try:
+                if not isinstance(self.elements_list[key], dict):
+                    key += 1
+                    raise TypeError
+            except TypeError:
+                print('Данные не являются словарем или в словаре нет id.')
+            else:
+                return self.elements_list[key]
+        else:
+            print('Данные не являются словарем или в словаре нет id.')
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -2,6 +2,7 @@
 import unittest
 from src.linked_list import Node, LinkedList
 
+
 class TestSrc(unittest.TestCase):
     def test_node_init(self):
         n1 = Node({'id': 1}, None)
@@ -15,7 +16,6 @@ class TestSrc(unittest.TestCase):
         ll.insert_beginning({'id': 0})
         self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> None")
 
-
     def test_insert_at_end(self):
         l = LinkedList()
         l.insert_at_end({'id': 2})
@@ -24,3 +24,26 @@ class TestSrc(unittest.TestCase):
         l.insert_beginning({'id': 0})
         l.insert_at_end({'id': 3})
         self.assertEqual(str(l), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+    def test_to_list(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        lst = ll.to_list()
+        self.assertEqual(lst[1], {'id': 1, 'username': 'lazzy508509'})
+
+    def test_get_data_by_id(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        self.assertEqual(ll.get_data_by_id(3), {'id': 3, 'username': 'mosh_s'})
+        l = LinkedList()
+        l.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        l.insert_at_end('idusername')
+        l.insert_at_end([1, 2, 3])
+        l.insert_at_end({'id': 2, 'username': 'mosh_s'})
+        self.assertEqual(l.get_data_by_id(2), {'id': 2, 'username': 'mosh_s'})
